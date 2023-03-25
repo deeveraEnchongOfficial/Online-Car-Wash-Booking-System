71e6524315ad938d76d6727b38f4be348ca69664
deeveraEnchongOfficial <deveraflorencio18@gmail.com>
2023-03-25 12:01:11 +0800

    modify initials

--- admin/config.php
+++ admin/config.php
@@ -4,7 +4,7 @@ ini_set('date.timezone','Asia/Manila');
 date_default_timezone_set('Asia/Manila');
 session_start();
 
-require_once('initialize.php');
+require_once('../initialize.php');
 require_once('classes/DBConnection.php');
 require_once('classes/SystemSettings.php');
 $db = new DBConnection;
